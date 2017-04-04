46d76ee5901f761ffbf7646d2b1690c026b0cbcb
Rad Poznyakov <rad.poznyakov@gmail.com>
2017-04-04 23:24:29 +0300

    single media done

--- app/js/custom.js
+++ app/js/custom.js
@@ -22,8 +22,7 @@ window.onload = function() {
     try{
       $('.album-list').masonry({
         itemSelector: '.album-item',
-        columnWidth: 80,
-        gutter: 19
+        gutter: 23
       });
     }catch(e){
       console.log(e);
