b54a75e4c70ee3e895dda65df58ee702dd8749a0
Rad Poznyakov <rad.poznyakov@gmail.com>
2017-04-04 12:24:20 +0300

    Ready news single

---
+++ app/js/custom.js
@@ -0,0 +1,17 @@
+$(function() {
+
+
+  // ==============  Parallax Single News ================ //
+  (function () {
+    var img = $('.parallax-news');
+    var backgroundURL = img.data("bg");
+    img.parallax({imageSrc: backgroundURL});
+  })();
+  // ==============  Parallax Single News Item ================ //
+
+
+
+
+
+
+});
