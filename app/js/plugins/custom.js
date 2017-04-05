6d99c3b51e35603aa2209577703f80add7066749
Rad Poznyakov <rad.poznyakov@gmail.com>
2017-04-05 17:18:43 +0300

    update project

--- app/js/plugins/custom.js
+++ app/js/plugins/custom.js
@@ -1,60 +1,77 @@
  $(function(){
+
+      try{
         var owl = $('#test');
         $('#test').owlCarousel({
-            loop:true,
-              autoplay:true,
-        autoplayTimeout:5000,
-        responsiveClass:true,
-        margin:0,
-        autoplayHoverPause:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    521:{
-                        items:2
-                    },
-                    751:{
-                        items:3
-                    },
-                    1000:{
-                        items:4
-                    }
-                }
-            });
+          loop:true,
+          autoplay:true,
+          autoplayTimeout:5000,
+          responsiveClass:true,
+          margin:0,
+          autoplayHoverPause:true,
+          responsive:{
+            0:{
+              items:1
+            },
+            521:{
+              items:2
+            },
+            751:{
+              items:3
+            },
+            1000:{
+              items:4
+            }
+          }
+        });
         $('.play').on('click',function(){
-            owl.trigger('play.owl.autoplay',[5000])
+          owl.trigger('play.owl.autoplay',[5000])
         })
         $('.stop').on('click',function(){
-            owl.trigger('stop.owl.autoplay')
+          owl.trigger('stop.owl.autoplay')
         })
+      }catch (e){
+        console.log(e);
+      }
+
+
+
         //////////////////////////////////////////////////////////////
-        var owl_index = $('#index-carousel');
-        $('#index-carousel').owlCarousel({
-        loop:true,
-        autoplay:true,
-        dots:false,
-        autoplayTimeout:6000,
-        responsiveClass:true,
-        margin:0,
-        smartSpeed:1000,
-        autoplayHoverPause:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:1
-                    },
-                    1000:{
-                        items:1
-                    }
-                }
-            });
-        $('.play').on('click',function(){
-            owl_index.trigger('play.owl_index.autoplay',[6000])
-        })
-        $('.stop').on('click',function(){
-            owl_index.trigger('stop.owl_index.autoplay')
-        })
+   try{
+     var owl_index = $('#index-carousel');
+     $('#index-carousel').owlCarousel({
+       loop:true,
+       autoplay:true,
+       dots:false,
+       autoplayTimeout:6000,
+       responsiveClass:true,
+       margin:0,
+       smartSpeed:1000,
+       autoplayHoverPause:true,
+       responsive:{
+         0:{
+           items:1
+         },
+         600:{
+           items:1
+         },
+         1000:{
+           items:1
+         }
+       }
+     });
+     $('.play').on('click',function(){
+       owl_index.trigger('play.owl_index.autoplay',[6000])
+     })
+     $('.stop').on('click',function(){
+       owl_index.trigger('stop.owl_index.autoplay')
+     })
+   }catch (e){
+        console.log(e);
+   }
+
+
+
+
+
     });
